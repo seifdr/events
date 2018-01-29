96e532b2575d67791cd99660f581b5bd727643ae
Duncan Seif <duncan.seif@hotmail.com>
2018-01-28 18:56:10 -0600

    Added in edit and save button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,85 @@
-var eForm = document.getElementById('registrar');
-var eInput = eForm.querySelector('input');
-eForm.addEventListener('submit', function (e) {
-    var ul = document.getElementById('invitedList');
+var eForm1 = document.getElementById('registrar');
+var eInput1 = eForm1.querySelector('input');
+var eUl = document.getElementById('invitedList');
+function createLI(text) {
     var li = document.createElement('li');
-    li.textContent = eInput.value;
-    // console.log( eInput.value );
-    ul.appendChild(li);
+    var eSpan = document.createElement('span');
+    eSpan.textContent = text;
+    li.appendChild(eSpan);
+    //create a label
+    var label = document.createElement('label');
+    label.textContent = "Confirmed";
+    //create checkbox
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    //append checkbox to the label
+    label.appendChild(checkbox);
+    //append label+checkbox to the list item
+    li.appendChild(label);
+    //create cremove button
+    var editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    li.appendChild(editBtn);
+    //create cremove button
+    var rmBtn = document.createElement('button');
+    rmBtn.textContent = 'Remove';
+    li.appendChild(rmBtn);
+    return li;
+}
+eForm1.addEventListener('submit', function (e) {
+    var eText = eInput1.value;
+    eInput1.value = '';
+    var li = createLI(eText);
+    //append list item to list  
+    eUl.appendChild(li);
     return e.preventDefault();
+});
+eUl.addEventListener('change', function (e) {
+    var checkbox = e.target;
+    var checked = checkbox.checked;
+    //need a reference to the checkbox. LI is the granparent, checkbox child of label, label is child of the li
+    var listItem = checkbox.parentNode.parentNode;
+    if (checked) {
+        listItem.className = 'responded';
+    }
+    else {
+        listItem.className = '';
+    }
+    // if(  )
+});
+eUl.addEventListener('click', function (e) {
+    //filter out elements that arent buttons
+    if (e.target.tagName === 'BUTTON') {
+        var li = e.target.parentNode;
+        var ul = li.parentNode;
+        if (e.target.textContent == 'Remove') {
+            ul.removeChild(li);
+        }
+        else if (e.target.textContent == 'Edit') {
+            //edit button
+            var eSpan = li.firstElementChild;
+            var spanInput = document.createElement('input');
+            spanInput.type = 'text';
+            spanInput.value = eSpan.textContent;
+            li.insertBefore(spanInput, eSpan);
+            li.removeChild(eSpan);
+            //change edit to save
+            e.target.textContent = 'Save';
+        }
+        else if (e.target.textContent == 'Save') {
+            alert('hello');
+            //save button
+            //get text input 
+            var spanInput = li.firstElementChild;
+            //use text input value to set span textContent
+            var eSpan = document.createElement('span');
+            eSpan.textContent = spanInput.value;
+            // add Span  
+            li.insertBefore(eSpan, spanInput);
+            //remove Input
+            li.removeChild(spanInput);
+            //change button text to edit
+            e.target.textContent = "Edit";
+        }
+    }
 });
--- ts/app.ts
+++ ts/app.ts
@@ -1,11 +1,106 @@
-const eForm = document.getElementById('registrar');
-const eInput = eForm.querySelector('input');
+const eForm1 = document.getElementById('registrar');
+const eInput1 = eForm1.querySelector('input');
+const eUl    = document.getElementById('invitedList');
 
-eForm.addEventListener('submit', (e) => {
-    const ul    = document.getElementById('invitedList');
+function createLI( text ){
     const li    = document.createElement('li');
-    li.textContent = eInput.value;
-    // console.log( eInput.value );
-    ul.appendChild(li);
-    return e.preventDefault();  
+    const eSpan  = document.createElement('span');
+    eSpan.textContent = text;
+    li.appendChild(eSpan);
+    //create a label
+    const label = document.createElement('label');
+    label.textContent = "Confirmed";
+
+    //create checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    //append checkbox to the label
+    label.appendChild(checkbox);  
+
+    //append label+checkbox to the list item
+    li.appendChild(label); 
+
+    //create cremove button
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    li.appendChild(editBtn);  
+
+    //create cremove button
+    const rmBtn = document.createElement('button');
+    rmBtn.textContent = 'Remove';
+    li.appendChild(rmBtn);  
+
+    return li;
+}
+
+
+eForm1.addEventListener('submit', (e) => {
+    const eText     = eInput1.value;
+    eInput1.value   = '';
+
+    const li = createLI( eText );
+    
+    //append list item to list  
+    eUl.appendChild(li);
+    return e.preventDefault();   
 }); 
+
+eUl.addEventListener('change', (e:any) => {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+
+    //need a reference to the checkbox. LI is the granparent, checkbox child of label, label is child of the li
+    const listItem = checkbox.parentNode.parentNode;
+
+    if( checked ){
+        listItem.className = 'responded';
+    } else {
+        listItem.className = '';
+    }
+
+    // if(  )
+});
+
+eUl.addEventListener('click', (e:any) => {
+    //filter out elements that arent buttons
+    if( e.target.tagName === 'BUTTON' ){
+        const li = e.target.parentNode;
+        const ul = li.parentNode;
+
+        if( e.target.textContent =='Remove' ){
+            ul.removeChild(li);
+        } else if( e.target.textContent =='Edit' ) {
+            //edit button
+            const eSpan     = li.firstElementChild;
+            const spanInput = document.createElement('input');
+            spanInput.type = 'text';
+            spanInput.value  = eSpan.textContent;
+            li.insertBefore(spanInput, eSpan);
+            li.removeChild(eSpan);
+
+            //change edit to save
+            e.target.textContent = 'Save';
+        } else if( e.target.textContent =='Save' ) {
+            alert('hello');
+            //save button
+            //get text input 
+            const spanInput = li.firstElementChild;
+
+            //use text input value to set span textContent
+            const eSpan = document.createElement('span');
+            eSpan.textContent = spanInput.value;
+
+            // add Span  
+            li.insertBefore( eSpan, spanInput );
+
+            //remove Input
+            li.removeChild( spanInput );
+            
+            //change button text to edit
+            e.target.textContent = "Edit";
+        }
+
+    }
+
+});
